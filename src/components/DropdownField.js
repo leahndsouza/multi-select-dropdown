63055fbab42036f29de05ab530e88236524a189c
Leahn Jude D'souza <leahnjd@gmail.com>
2023-03-25 21:25:07 +0530

    Dropdown state inegration

--- src/components/DropdownField.js
+++ src/components/DropdownField.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
+import { stateData } from "./StateData";
+
 const DropdownField = () => {
     const [dropdownActive, setDropdownActive] = useState(false);
 
     return(
-        <>
+        <fieldset className="dropdown">
         <button className="dropdown-field" onClick={() => setDropdownActive((prevState) => !prevState)}>
             Select required states
         </button>
-        {dropdownActive ? <div className="state-panel">Hello World</div> : null}
-        </>
+        {dropdownActive ? <div className="state-panel">
+            {stateData.map((list, index) => (
+                <div>
+                    <input id={list.code} type="checkbox" />
+                    <label htmlFor={list.code} >{list.name}</label>
+                </div>
+            ))}
+        </div> : null}
+        </fieldset>
     )
 }
 
